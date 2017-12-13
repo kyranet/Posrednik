29ca31f9173209ae6b0ca0d6d9dd2c2c21ef6045
Ethoarx <moddersgithub@gmail.com>
2017-12-13 07:19:34 +0100

    finer perm levels

--- posrednik.js
+++ posrednik.js
@@ -1,11 +1,27 @@
-const klasa = require('klasa');
+const { Client, PermissionLevels } = require('klasa');
 const config = require('./config.json');
 
-const client = new klasa.Client({
-    clientOptions: {},
-    prefix: 'p!',
-    cmdPrompt: true,
-    cmdEditing: true
-});
+const permissionLevels = new PermissionLevels()
+    .addLevel(0, false, () => true)
+    .addLevel(3, false, (client, msg) => msg.guild && msg.member.permissions.has('KICK_MEMBERS'))
+    .addLevel(4, false, (client, msg) => msg.guild && msg.member.permissions.has('BAN_MEMBERS'))
+    .addLevel(5, false, (client, msg) => msg.guild && msg.member.permissions.has('MANAGE_GUILD'))
+    .addLevel(6, false, (client, msg) => msg.guild && msg.member.permissions.has('ADMINISTRATOR'))
+    .addLevel(9, true, (client, msg) => msg.author === client.owner)
+    .addLevel(10, false, (client, msg) => msg.author === client.owner);
 
-client.login(config.token);
+class client extends Client {
+
+    constructor() {
+        super({
+            clientOptions: {},
+            prefix: 'p!',
+            cmdPrompt: true,
+            cmdEditing: true,
+            permissionLevels
+        });
+    }
+
+}
+
+new client().login(config.token);
