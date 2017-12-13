b8e5363240cbd8a9d1dd4ac542ceef5f96221e6d
Ethoarx <moddersgithub@gmail.com>
2017-12-13 16:42:53 +0100

    small perm level tweak

--- posrednik.js
+++ posrednik.js
@@ -3,9 +3,9 @@ const config = require('./config.json');
 
 const permissionLevels = new PermissionLevels()
     .addLevel(0, false, () => true)
+    .addLevel(2, false, (client, msg) => msg.guild && msg.member.permissions.has('MANAGE_GUILD'))
     .addLevel(3, false, (client, msg) => msg.guild && msg.member.permissions.has('KICK_MEMBERS'))
     .addLevel(4, false, (client, msg) => msg.guild && msg.member.permissions.has('BAN_MEMBERS'))
-    .addLevel(5, false, (client, msg) => msg.guild && msg.member.permissions.has('MANAGE_GUILD'))
     .addLevel(6, false, (client, msg) => msg.guild && msg.member.permissions.has('ADMINISTRATOR'))
     .addLevel(9, true, (client, msg) => msg.author === client.owner)
     .addLevel(10, false, (client, msg) => msg.author === client.owner);
